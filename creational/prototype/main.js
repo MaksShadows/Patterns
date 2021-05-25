9f99343d6b0cb9fa9f2f19c807118a8e0d31b921
MaksShadows <Lykanenko1995@gmail.com>
2021-05-25 18:12:42 +0300

    add

--- creational/prototype/main.js
+++ creational/prototype/main.js
@@ -3,10 +3,12 @@
 в подробности их реализации.
 */
 
+
 class Book {
     constructor() {}
-}
 
+    clone = () => {};
+}
 
 class BookPrototype extends Book {
 
@@ -19,20 +21,58 @@ class BookPrototype extends Book {
     }
 
     clone = () => {
-        const book = new BookPrototype();
-        book.genre = proto.genre;
-        book.author = proto.author;
-        book.name = proto.name;
-        book.years = proto.years;
-        return book;
+
+        return new BookPrototype(this.genre, this.author, this.name, this.years)
+        // const book = new BookPrototype();
+        // book.genre = proto.genre;
+        // book.author = proto.author;
+        // book.name = proto.name;
+        // book.years = proto.years;
+        // return book;
 
     }
 }
 
-
 const proto = new BookPrototype("Детектив", "Стивен Кинг", "Мистер Мерседес", "2020");
 const book = proto.clone();
 console.log(book);
+
+
+
+
+
+
+
+// class Book {
+//     constructor() {}
+// }
+
+
+// class BookPrototype extends Book {
+
+//     constructor(genre, author, name, years) {
+//         super();
+//         this.genre = genre;
+//         this.author = author;
+//         this.name = name;
+//         this.years = years;
+//     }
+
+//     clone = () => {
+//         const book = new BookPrototype();
+//         book.genre = proto.genre;
+//         book.author = proto.author;
+//         book.name = proto.name;
+//         book.years = proto.years;
+//         return book;
+
+//     }
+// }
+
+
+// const proto = new BookPrototype("Детектив", "Стивен Кинг", "Мистер Мерседес", "2020");
+// const book = proto.clone();
+// console.log(book);
 
 
 
