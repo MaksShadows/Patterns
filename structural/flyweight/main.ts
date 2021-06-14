f877a3c7e43aa343a29463dd58985c2628a5a5df
MaksShadows <Lykanenko1995@gmail.com>
2021-06-14 21:56:46 +0300

    add

---
+++ structural/flyweight/main.ts
@@ -0,0 +1,40 @@
+ interface Flyweight {
+    operation(s: String): void;
+}
+
+ class ConcreteFlyweight implements Flyweight {
+    private instrinsicState: String;
+
+    constructor(instrinsicState: String) {
+        this.instrinsicState = instrinsicState;
+    }
+
+    public operation(s: String): void {
+        console.log("create of ConcreteFlyweight", s, " is being called!");
+    }
+}
+
+
+export class FlyweightFactory {
+
+    private fliesMap: { [s: string]: Flyweight; } = <any>{};
+
+    constructor() { }
+
+    public getFlyweight(key: string): Flyweight {
+
+        if (this.fliesMap[key] === undefined || null) {
+            this.fliesMap[key] = new ConcreteFlyweight(key);
+        }
+        return this.fliesMap[key];
+    }
+}
+
+
+ var factory: FlyweightFactory   = new FlyweightFactory(),
+
+ conc1  = <ConcreteFlyweight>factory.getFlyweight("conc1"),
+ conc2  = <ConcreteFlyweight>factory.getFlyweight("conc2");
+
+ conc1.operation("1");
+ conc2.operation("2");
